cf35f34bc6001cf6e10edd62caf9f7b176281fc3
Kishan Patel <kvpatel6351665793@gmail.com>
2026-07-14 00:53:35 +0530

    creating reusable recat components

---
+++ src/components/Login.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link , useNavigate } from 'react-router-dom'
+import { useDispatch } from 'react-redux'
+import authService from '../appwrite/auth'
+import 
+
+function Login() {
+  return (
+    <div>Login</div>
+  )
+}
+
+export default Login
